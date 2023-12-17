aa42cc9c4dbb861315f9afc2311bdc8c1994b785
Marat Shaidullin <marat.dev@bgsoft.info>
2023-12-17 17:26:22 +0100

    Add project files.

---
+++ BoolToObjectConverter.cs
@@ -0,0 +1,40 @@
+﻿using Avalonia.Data.Converters;
+using System.Globalization;
+using System;
+
+namespace AvaloniaSplitViewIssueDemo;
+
+public class BoolToObjectConverter : BoolToObjectConverter<object>
+{
+}
+
+public class BoolToObjectConverter<TObject> : IValueConverter
+{
+    public TObject? FalseObject { get; set; }
+
+    public TObject? TrueObject { get; set; }
+
+    public object? Convert(object? value, Type? targetType, object? parameter, CultureInfo? culture)
+    {
+        if (value is bool result)
+        {
+            return result ? TrueObject : FalseObject;
+        }
+        throw new ArgumentException("Value is not a valid boolean", nameof(value));
+    }
+
+    public object ConvertBack(object? value, Type? targetType, object? parameter, CultureInfo? culture)
+    {
+        if (value is TObject result)
+        {
+            return result.Equals(TrueObject);
+        }
+
+        if (default(TObject) == null && value == null && TrueObject == null)
+        {
+            return true;
+        }
+
+        throw new ArgumentException($"Value is not a valid {typeof(TObject).Name}", nameof(value));
+    }
+}
---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace AvaloniaSplitViewIssueDemo;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
